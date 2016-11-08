ebb187a618c3be33bc83b086eaf69aaf0ff71001
Nick Bereza <nbereza@authair.com>
2016-11-08 11:38:52 -0500

    Initial Commit

---
+++ nuget_EntityInterfaces.bat
@@ -0,0 +1,10 @@
+﻿@mkdir Nugets
+@mkdir Nugets\EntityInterfaces
+@mkdir Nugets\EntityInterfaces\lib
+@mkdir Nugets\EntityInterfaces\lib\net40
+
+copy "bin\Release\EntityInterfaces.dll" Nugets\EntityInterfaces\lib\net40\
+
+copy "EntityInterfaces.nuspec" Nugets\EntityInterfaces\
+
+nuget pack Nugets\EntityInterfaces\EntityInterfaces.nuspec
